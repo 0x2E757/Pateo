f5c8a9be2dfa5f945219a7eb11297333ce891383
Eric <0x2E757@gmail.com>
2022-08-11 19:20:38 +0300

    Added default exports, updated version to v1.1.2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export { IWrapper, StaticWrapper, IStaticWrapper, DynamicWrapper, IDynamicWrapper, subscribe } from "./wrappers";
+import {
+    IWrapper,
+    StaticWrapper,
+    IStaticWrapper,
+    DynamicWrapper,
+    IDynamicWrapper,
+    subscribe,
+} from "./wrappers";
+
+export {
+    IWrapper,
+    StaticWrapper,
+    IStaticWrapper,
+    DynamicWrapper,
+    IDynamicWrapper,
+    subscribe,
+};
+
+export default {
+    StaticWrapper,
+    DynamicWrapper,
+    subscribe,
+};
